753f768393396c82be61f5fa336d39b19c78310f
sadiamazhar-hp <80788093+sadiamazhar-hp@users.noreply.github.com>
2025-03-08 13:49:23 +0500

    Add project files.

---
+++ Configuratons/Services/Services.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BookMyHall.Configurations.Services
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Register application services here
+            //services.AddScoped<IBookingService, BookingService>();
+            
+
+            // Add more services as needed
+            return services;
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookMyHall.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
